9ba4ba5d11027a47347e06246c41b3cffc8645fe
Yash Mahalwal <yashmahalwal@gmail.com>
2019-12-26 17:21:03 +0530

    feat: made component

--- src/components/promise-sync.tsx
+++ src/components/promise-sync.tsx
@@ -1,8 +1,52 @@
-import React from "react";
+import React, { useEffect } from "react";
 
-const PromiseSync: React.FunctionComponent = () => {
-    return <h1>Hello World</h1>;
+interface Props {
+    count: number;
+    fetchNext: (
+        cursor: number,
+        count: number
+    ) => Array<Promise<React.ReactNode>>;
+    error: React.ReactNode;
+    render: (
+        elements: React.ReactNode[],
+        loadNext: () => void,
+        loading?: boolean
+    ) => React.ReactNode | React.ReactNode[];
+}
+
+const PromiseSync: React.FunctionComponent<Props> = ({
+    count,
+    error,
+    fetchNext,
+    render
+}) => {
+    const [cursor, setCursor] = React.useState<number>(0);
+    const [loading, setLoading] = React.useState<boolean>(false);
+    const [elements, setElements] = React.useState<React.ReactNode[]>([]);
+
+    const synchronize = (): void => {
+        setLoading(true);
+        const arr = fetchNext(cursor, count);
+
+        Promise.all(
+            arr.map(p =>
+                p.catch((err: Error) => {
+                    console.log(err.message);
+                    return error;
+                })
+            )
+        ).then((values: React.ReactNode[]) => {
+            setElements(e => [...e, ...values]);
+            setLoading(false);
+            setCursor(c => c + count);
+        });
+    };
+
+    useEffect(() => {
+        synchronize();
+    }, []);
+
+    return <>{render(elements, synchronize, loading)}</>;
 };
-
 PromiseSync.displayName = "PromiseSync";
 export default PromiseSync;
--- src/stories/promise-sync.stories.tsx
+++ src/stories/promise-sync.stories.tsx
@@ -5,7 +5,49 @@ export default {
     title: "Promise Sync"
 };
 
-export const promiseSync = (): React.ReactNode => <PromiseSync />;
+const a = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14];
+
+const temp = (index: number): Promise<number> =>
+    new Promise((resolve, reject) => {
+        if (index >= 0 && index < a.length && Number.isInteger(index))
+            setTimeout(() => resolve(a[index]), Math.random() * 1000);
+        else reject(new Error("Invalid index supplied"));
+    });
+
+const fetchNext = (
+    cursor: number,
+    count: number
+): Promise<React.ReactNode>[] => {
+    const tempArr = [];
+
+    for (let i = 0; i < count; i++) {
+        tempArr.push(temp(cursor + i));
+    }
+    return tempArr;
+};
+
+export const promiseSync = (): React.ReactNode => (
+    <PromiseSync
+        count={2}
+        error={null}
+        fetchNext={fetchNext}
+        render={(elements, loadNext, loading): React.ReactNode => {
+            return (
+                <>
+                    {elements.map((e, i) => (
+                        <React.Fragment key={i}>
+                            {e}
+                            <br />
+                        </React.Fragment>
+                    ))}
+                    <button disabled={loading} onClick={(): void => loadNext()}>
+                        {loading ? "Loading..." : "Next"}
+                    </button>
+                </>
+            );
+        }}
+    />
+);
 
 promiseSync.story = {
     name: "Promise-Sync"
